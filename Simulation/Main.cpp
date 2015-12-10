729a4c81039ec38354126df91d45a97ef14823d5
zqmillet <zqmillet@qq.com>
2015-12-10 21:24:22 +0800

    Verified by an Example

--- Simulation/Main.cpp
+++ Simulation/Main.cpp
@@ -4,54 +4,66 @@ using namespace std;
 
 int main()
 {    
-    Node A("A", Attack);
-    Node B("B", Attack);
-    Node C("C", Attack);
-    Node D("D", Function);
+    Node PT("Party");
+    Node HO("Hangover");
+    Node BT("Brain Tumor");
+    Node HA("Headache");
+    Node SA("Smell Alcohol");
+    Node PX("Pos Xray");
 
-    BayesianNetwork BayesianNetwork;
+    HO.Add(PT);
+    HA.Add(HO, BT);
+    SA.Add(HO);
+    PX.Add(BT);
 
-
-    D.Add(A);
-    A.Add(B, C);
-
-    A.Probabilities = {
-    /*
-    C   F       F       T       T
-    B   F       T       F       T
-    */
-        0.01,   0.9,    0.5,    0.99
+    PT.Probabilities = {
+        0.2
     };
 
-    B.Probabilities = {
-        0.01
-    };
-
-    C.Probabilities = {
+    BT.Probabilities = {
         0.001
     };
 
-    D.Probabilities = {
+    HO.Probabilities = {
     /*
-    A   F       T
+    PT  F       T
     */
-        0.2,    0.5
+        0,      0.7
     };
 
-    BayesianNetwork.AddNode(B, C, D, A);
+    SA.Probabilities = {
+    /*
+    HO  F       T
+    */
+        0.1,    0.8  
+    };
+
+    PX.Probabilities = {
+    /*
+    BT  F       T
+    */
+        0.01,   0.98
+    };
+
+    HA.Probabilities = {
+    /*
+    BT  F       F       T       T
+    HO  F       T       F       T
+    */
+        0.02,   0.7,    0.9,    0.99
+    };
+
+    BayesianNetwork BayesianNetwork;
+    BayesianNetwork.AddNode(PT, HO, BT, HA, SA, PX);
 
     if (!BayesianNetwork.Initialize())
         return EXIT_FAILURE;
-    
-    //BayesianNetwork.AddEvidence(A, Unhappened);
-    BayesianNetwork.AddEvidence(C, Happened);
+
+    BayesianNetwork.AddEvidence(HA);
     BayesianNetwork.Inference();
     BayesianNetwork.PrintProbabilities(OrderByName, Ascend);
-
-    /*BayesianNetwork.RemoveEvidence(B);
-    BayesianNetwork.Inference();
-    BayesianNetwork.PrintProbabilities(OrderByName, Ascend);*/
 
     system("pause");
     return EXIT_SUCCESS;
 }
+
