86aebb70c05e69dc4c3830bb5ea7b0cc6ece2252
zqmillet <zqmillet@qq.com>
2015-12-19 21:37:38 +0800

    Addition of Functions of Class Process

--- +Classes/@Process/AddInputs.m
+++ +Classes/@Process/AddInputs.m
@@ -1,7 +1,17 @@
-function [ output_args ] = AddInputs( input_args )
-%ADDINPUTS Summary of this function goes here
-%   Detailed explanation goes here
+function AddInputs(obj, varargin)
 
-
+    if (nargin < 2)
+        error(Enumerations.ErrorType.InputParameterNumberError);
+    end
+    
+    for i = 1:numel(varargin)
+        if (~isa(varargin{i}, Enumerations.ClassType.Product))
+            error(Enumerations.ErrorType.InputParameterTypeError);
+        end
+        
+        if (~obj.ExistInput(varargin{i}))
+            obj.Inputs{1, numel(obj.Inputs) + 1} = varargin{i};
+        end
+    end
 end
 
--- +Classes/@Process/AddOutputs.m
+++ +Classes/@Process/AddOutputs.m
@@ -1,7 +1,18 @@
-function [ output_args ] = AddOutputs( input_args )
-%ADDOUTPUTS Summary of this function goes here
-%   Detailed explanation goes here
+function AddOutputs(obj, varargin)
 
+    if (nargin < 2)
+        error(Enumerations.ErrorType.InputParameterNumberError);
+    end
+    
+    for i = 1:numel(varargin)
+        if (~isa(varargin{i}, Enumerations.ClassType.Product))
+            error(Enumerations.ErrorType.InputParameterTypeError);
+        end
+        
+        if (~obj.ExistOutput(varargin{i}))
+            obj.Outputs{1, numel(obj.Outputs) + 1} = varargin{i};
+        end
+    end
 
 end
 
