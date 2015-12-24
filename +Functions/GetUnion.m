705d90182302bc78dfa9b5e2ff29645315ec5380
zqmillet <zqmillet@qq.com>
2015-12-24 20:41:07 +0800

    Modification of Function GetUnion

--- +Functions/GetUnion.m
+++ +Functions/GetUnion.m
@@ -2,6 +2,6 @@ function UnionArray = GetUnion(varargin)
     if (nargin == 0)
         error(Enumerations.ErrorType.InputParameterNumberError);            
     end
-    UnionArray = Functions.UniqueCell([varargin{1:end}]);
+    UnionArray = Functions.UniqueCell([varargin{:}]);
 end
 
